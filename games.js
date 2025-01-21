c3737743ba0174e0efabcf61dc52399360cdfc4b
WilliamAlvesOliveira <27will.ramone@gmail.com>
2025-01-21 12:45:11 -0300

    game window criada

---
+++ games.js
@@ -0,0 +1,9 @@
+function openwindow(){
+    let gamescreen = document.querySelector(".games");
+    gamescreen.style.display = "flex";
+}
+
+function closewindow(){
+    let gamescreen = document.querySelector(".games");
+    gamescreen.style.display = "none";
+}
